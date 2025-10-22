f5e3c095d2fca540c1d6254f3b71566cc17b87c8
claw1405 <46711240+claw1405@users.noreply.github.com>
2025-10-22 14:43:16 +0100

    change bullet colour to red so they are more visible

--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ class Settings :
         self.bullet_speed = 2.0
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_colour = (60, 60, 60)
+        self.bullet_colour = (255, 0, 0)
 
         #set the spaceships speed
         self.ship_speed = 1.5
